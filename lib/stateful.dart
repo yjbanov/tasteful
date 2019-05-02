ae4501c08c61c21d3e32ca89423e8ea77c7375c8
Yegor Jbanov <yjbanov@google.com>
2019-05-01 21:43:07 -0700

    docs; markNeedsBuild in update; ==

--- lib/stateful.dart
+++ lib/stateful.dart
@@ -1,29 +1,57 @@
 import 'package:flutter/widgets.dart';
 
+/// A lighter-weight stateful widget.
+/// 
+/// The state is represented by an immutable object of type [S].
+/// The initial state value is obtained by calling [createInitialState]
+/// when the widget is built for the first time.
+/// 
+/// When inflating the widget, the element calls [build] passing
+/// a context object and current state value. Widget properties
+/// together with the state value provide information for building
+/// child widgets.
+/// 
+/// To transition from the current state to the next state, call
+/// `context.setState` passing it a new object of type [S]. The
+/// framework automatically rebuild the widget if the new state
+/// value is not equal to the old value. State values are compared
+/// using the `==` operator.
+// TODO(yjbanov): I wish S could be marked with @immutable.
 abstract class Stateful<S> extends Widget {
   Stateful({Key key}) : super(key: key);
 
+  /// Creates the initial state value.
   @protected
-  S createState() => null;
+  S createInitialState() => null;
+
+  /// Same as [StatelessWidget.build], but with a couple of differences:
+  /// 
+  /// * [StatefulBuildContext] is passed instead of [BuildContext].
+  /// * This method also receives a `state` parameter used as another source
+  ///   of configuration.
+  @protected
+  Widget build(StatefulBuildContext context, S state);
 
   @override
   Element createElement() {
     return _StatefulElement<S>(this);
   }
-
-  @protected
-  Widget build(StatefulBuildContext context, S state);
 }
 
 typedef SetStateCallback = void Function();
 
+/// [BuildContext] that also manages a [Stateful] widget's state.
 abstract class StatefulBuildContext<S> extends BuildContext {
+  /// Transitions a [Stateful] widget to a `newState`.
+  /// 
+  /// If `newState` is not equal (using `==`) to the previous state value, the
+  /// framework rebuilds the widget.
   void setState(S newState);
 }
 
 class _StatefulElement<S> extends ComponentElement implements StatefulBuildContext<S> {
   _StatefulElement(Stateful<S> widget)
-      : _state = widget.createState(),
+      : _state = widget.createInitialState(),
         super(widget);
 
   @override
@@ -35,13 +63,17 @@ class _StatefulElement<S> extends ComponentElement implements StatefulBuildConte
 
   @override
   void setState(S newState) {
+    final S oldState = _state;
     _state = newState;
-    markNeedsBuild();
+    if (oldState != newState) {
+      markNeedsBuild();
+    }
   }
 
   @override
   void update(Stateful newWidget) {
     super.update(newWidget);
+    markNeedsBuild();
     rebuild();
   }
 
--- test/stateful_test.dart
+++ test/stateful_test.dart
@@ -4,21 +4,32 @@ import 'package:stateful/stateful.dart';
 
 void main() {
   testWidgets('Stateful', (WidgetTester tester) async {
-    await tester.pumpWidget(_Counter());
+    // Initial build, with initial state.
+    await tester.pumpWidget(_Counter('Hello'));
     expect(find.text('Hello, 0!'), findsOneWidget);
+
+    // Change state independently from the widget.
     await tester.tap(find.text('Increment'));
     await tester.pumpAndSettle();
     expect(find.text('Hello, 1!'), findsOneWidget);
+
+    // Update widget independently from state.
+    await tester.pumpWidget(_Counter('Bonjour'));
+    expect(find.text('Bonjour, 1!'), findsOneWidget);
   });
 }
 
 class _Counter extends Stateful<int> {
-  int createState() => 0;
+  _Counter(this.greeting);
+
+  final String greeting;
+
+  int createInitialState() => 0;
 
   @override
   Widget build(StatefulBuildContext context, int state) {
     return Boilerplate(Column(children: [
-      Text('Hello, $state!'),
+      Text('$greeting, $state!'),
       GestureDetector(
         onTap: () {
           context.setState(state + 1);
